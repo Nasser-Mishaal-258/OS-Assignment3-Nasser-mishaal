6503a82fa949b55934fa20fe52b37f0d79d1f7bc
nasser <445050258@std.psau.edu.sa>
2026-05-02 01:22:24 +0300

    protect completedProcessCount

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -63,7 +63,12 @@ class SharedResources {
     // Method to increment completed process counter
     public static void incrementCompletedProcess() {
         // TODO: Protect this critical section with a lock
-        completedProcessCount++;
+                    counterLock.lock();
+                try {
+                    completedProcessCount++;
+                } finally {
+                    counterLock.unlock();
+                }
     }
 
     // Method to add waiting time
